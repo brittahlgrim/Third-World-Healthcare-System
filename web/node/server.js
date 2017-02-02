a91748c72f43d3ed27461c46fefea29f447164e6
Ben Durette <bendurette12@pcp086319pcs.mu.edu>
2017-02-01 22:32:57 -0600

    Connected node.js to mysql database

--- web/node/server.js
+++ web/node/server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var http = require('http');
 var https = require('https');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host		: 'localhost',
+	user		: 'root',
+	password	: 'topeno',
+	database	: 'test'
+});
 
 var app = express();
 var path = require('path');
 
+connection.connect(function(err){
+	if(!err) {
+		console.log("Database is connected ... nn");
+	} else {
+		console.log("Error connecting database ... nn");
+	}
+});
+/*
+app.get("/",function(req,res){
+	connection.query('SELECT * FROM employees;',
+		function(err, rows, fields) {
+			connection.end();
+			if(!err)
+				console.log('The solution is: ', rows);
+			else
+				console.log('Error while performing Query.');
+		});
+});
+*/
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/../static/views/home.html'));
 });
