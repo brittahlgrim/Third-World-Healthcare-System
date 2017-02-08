6fce558dfc98dedeb71a56bb7b0b3f99dc333657
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-02-08 12:55:50 -0600

    I don't think I added everything needed...

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Third-World-Healthcare-System
 Senior Design project to find a solution to improve the healthcare systems in underdeveloped countries.
+
+At the moment, it is a Node.js web application with an Angular front-end.
+
+In this base directory, run "./build.sh -p {password}" to run the server, set up the test db, and open a web page.
+This build script will do an npm install on everything in ./web/node/required-npm-packages . If you want to skip this process, add an -s flag. i.e.
+"./build.sh -p {password} -s"
--- build.sh
+++ build.sh
@@ -5,9 +5,26 @@
 #-----
 
 output=/dev/stdout
-if [ "$1" = "-q" ]
+skip_npm_update=false
+db_root_password="passwords cant have spaces"
+while getopts "p::s" opt; do
+  case $opt in
+  p)
+      db_root_password=$OPTARG
+      ;;
+  s)
+      skip_npm_update=true
+      ;;
+  esac
+done
+
+echo "skip=$skip_npm_update"
+
+if [ "$db_root_password" = "passwords cant have spaces" ]
 then
-	output=/dev/null
+	echo "ERROR: Enter password with -p flag"
+	echo "    (ex. ./build.sh -p foobar)"	
+	exit 127
 fi
 
 #-----
@@ -43,18 +60,56 @@ fi
 
 echo "Using $open_node_command for opening node..." > $output
 
-#Install every npm package in the file
-file="./web/node/required-npm-packages.txt"
-while IFS= read -r line
-do
-	echo "Checking if $line npm package is installed..." > $output
-	npm_output="$(npm install $line)"
-	if [ $? -ne 0 -a $? -ne 1 ]
-	then
-		echo "Error: couldn't install $line npm package..."
-		exit 127
-	else
-		echo "$npm_output" > $output
-	fi
+#check if mysql is installed
+echo "Checking if mysql is installed..." > $output
+tmp="$(mysql --help)"
+if [ $? -ne 0 -a $? -ne 1 ]
+then
+	echo "Error: couldn't find mysql on the system. Do you have it installed?"
+	exit 127
+fi
 
-done<"$file"
+#do an npm update for all specified files
+if [ "$skip_npm_update" = false ]
+then
+	./web/node/do-npm-update.sh
+fi
+
+#-----
+#SET UP MYSQL DB AND NODE SERVER
+#-----
+
+echo "Attempting to connect to db..." > $output
+
+#delete any previous database, remake it
+mysql --user=root --password=$db_root_password -e "drop database twhs_test_db"
+mysql --user=root --password=$db_root_password -e "create database twhs_test_db"
+mysql --user=root --password=$db_root_password twhs_test_db < ./db/records_db_table_structure.sql
+
+echo "Attempting to start node server..." > $output
+$open_node_command ./web/node/server.js &
+nodeserver_pid=$!
+sleep 5
+
+echo "Attempting to open web page... (Note: this might not work)" > $output
+
+#-----
+#DEPLOY BROWSER PAGE AND WAIT FOR USER TO CLOSE US
+#-----
+tmp="$(open http://localhost:8080)"
+if [ $? -ne 0 ]
+then
+	echo "***ERROR*** Open command didn't work. Attempting firefox, but you should be able to open any browser http://localhost:8080"
+	firefox http://localhost:8080
+fi
+
+echo -e "\t\t-----\n\t\t*****\n\t\t-----\nEverything set up successfully! Press enter to quit...\n\t\t-----\n\t\t*****\n\t\t-----"
+read tmp
+
+echo "Quiting... removing the Db instance..." > $output
+mysql --user=root --password=$db_root_password -e "drop database twhs_test_db"
+
+echo "removing node server..." > $output
+kill $nodeserver_pid
+
+echo "Done!"
