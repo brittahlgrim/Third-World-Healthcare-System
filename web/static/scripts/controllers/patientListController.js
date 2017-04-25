aecc734556dcfaf872b48580599c4b6abd9b3931
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-04-25 10:44:11 -0500

    Added Basic sort by name functionaliy

--- web/static/scripts/controllers/patientListController.js
+++ web/static/scripts/controllers/patientListController.js
@@ -5,7 +5,20 @@ angular.module('myApp').controller('patientListCtrl',
 			window.location.href = "/patientInfo?patientID=" + patientID;
 		};
 
-
+        $scope.sortPatientsBy = function(someField){
+            $scope.names.sort((a,b) => {
+                var nameA=a.Name.toLowerCase(), nameB=b.Name.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0});
+            //$scope.$apply();
+            console.log("This is the new $scope.names =>");
+            for(var i=0; i<$scope.names.length; i++){
+                console.log($scope.names[i].Name);
+            }
+        };
 
 
 //Trial function to get risk factors from the mysql table
