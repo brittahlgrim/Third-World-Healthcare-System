18a4636d38e33a278c60ef2628d6e2a0a34a8aad
Ubuntu <ubuntu@ip-172-31-5-239.us-west-2.compute.internal>
2017-05-04 04:45:08 +0000

    Cleaned up err and out log file tracking, added sudo to start and stop scripts

--- stopForever.sh
+++ stopForever.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
      
 # Invoke the Forever module (to STOP our Node.js server).
-./node_modules/forever/bin/forever stop ./web/node/server.js
+sudo ./node_modules/forever/bin/forever stop ./web/node/server.js
 
